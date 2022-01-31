8670cfe886cb0577b3ce8f49a38857b193185135
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2022-01-31 15:35:02 -0500

    fix testing

--- ui/src/app/components/dashboard/dashboard.component.spec.ts
+++ ui/src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,24 +1,50 @@
+import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { ApiService } from 'src/app/services/api.service';
 import { DashboardComponent } from './dashboard.component';
+import { WeatherSummary } from 'src/app/services/api.service.types';
 
 describe('DashboardComponent', () => {
     let component: DashboardComponent;
     let fixture: ComponentFixture<DashboardComponent>;
 
+    let apiService: ApiService;
+
+    const mockWeatherSummary: WeatherSummary = {
+        guid: 'd74b4f8c-48a3-4a4b-9b89-a3a560bc309a',
+        city: 'Indianapolis',
+        state: 'IN',
+        description: 'sunny',
+        currentHumidity: 80,
+        currentWindSpeed: 10,
+        currentTemperature: 75,
+    };
+
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [DashboardComponent],
+            imports: [RouterTestingModule, HttpClientModule],
         }).compileComponents();
+
+        apiService = TestBed.inject(ApiService);
     });
 
     beforeEach(() => {
+        spyOn(apiService, 'getWeatherSummary').and.returnValue(of([mockWeatherSummary]));
+
         fixture = TestBed.createComponent(DashboardComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
 
     it('should create', () => {
-        expect(component).toBeTruthy();
+        const element = fixture.debugElement.query(By.css('.city'));
+        expect(component.weatherSummaries).toEqual([mockWeatherSummary]);
+        const city = element.nativeElement as HTMLSpanElement;
+        expect(city.textContent).toBe('Indianapolis');
     });
 });
--- ui/src/app/components/location/location.component.spec.ts
+++ ui/src/app/components/location/location.component.spec.ts
@@ -1,25 +1,27 @@
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LocationComponent } from './location.component';
 
 describe('LocationComponent', () => {
-  let component: LocationComponent;
-  let fixture: ComponentFixture<LocationComponent>;
+    let component: LocationComponent;
+    let fixture: ComponentFixture<LocationComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LocationComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [LocationComponent],
+            imports: [RouterTestingModule, HttpClientModule],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LocationComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LocationComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
